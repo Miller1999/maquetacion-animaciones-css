00948599b5c9623f2e3d9afb77f9b3dc1bff529f
Miller Arias <millerjavierarias@gmail.com>
2024-04-04 13:08:34 -0500

    Maquetacion inicial

---
+++ main.js
@@ -0,0 +1,37 @@
+import "normalize.css";
+
+document.querySelector("#app").innerHTML = `
+  <section class="container">
+    <nav class="navbar">
+      <div class="navbar__content">
+      <img/>
+        <ul>
+          <li><a href="#">Exclusivos</a></li>
+          <li><a href="#">Nuevos</a></li>
+          <li><a href="#">Gift Cards</a></li>
+          <li><a href="#">Find a Store</a></li>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <h1>Diseña tu <strong>Super Heroe!</strong></h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, harum. Iure omnis veritatis recusandae quo ducimus facere quisquam ratione placeat illo doloremque? Veritatis ratione pariatur tempore mollitia quae, nesciunt nostrum.</p>
+    </main>
+    <aside>
+      <p>SuperMan</p>
+      <img/>
+      <div></div>
+    </aside>
+    <footer>
+      <div>
+        <img/>
+      </div>
+    </footer>
+  </section>
+  <section class="loader">
+    <div></div>
+    <div></div>
+    <div></div>
+  </section>
+  <section class="modal"></section>
+`;
